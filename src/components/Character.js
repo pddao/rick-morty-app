543a4a56467f0e4c92094a4ba7ddcee774098954
d_dao <dung.dao@hotmail.de>
2021-05-14 10:33:32 +0200

    Add search function to App.js

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,11 @@
-function Character({character}) {
-
-    return <section>
-        <h3>
-            {character.name}
-        </h3>
-        <img src={character.image}/>
-        <p>
-            {character.species}
-        </p>
+function Character({ character }) {
+  return (
+    <section>
+      <h3>{character.name}</h3>
+      <img src={character.image} />
+      <p>{character.species}</p>
     </section>
+  );
 }
 
 export default Character;
